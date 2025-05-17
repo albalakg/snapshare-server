40c2265fdd3e3f5c8de504260532f046364c894f
Blacky <gal.blacky@gmail.com>
2025-05-17 18:17:05 +0300

    fix(event started mail)

--- resources/views/mails/eventStarted.blade.php
+++ resources/views/mails/eventStarted.blade.php
@@ -7,9 +7,9 @@
     <div style="background-color: #f8f9fa; padding: 15px; border-radius: 5px; margin: 20px 0;">
         <h3>פרטי האירוע:</h3>
         <p><strong>שם האירוע:</strong> {{ $event->name }}</p>
-        <p><strong>תאריך:</strong> {{ \Carbon\Carbon::parse($event->starts_at)->format('dd/mm/YYYY') }}</p>
-        <p><strong>שעת התחלה:</strong> {{ \Carbon\Carbon::parse($event->starts_at)->format('H:i') }}</p>
-        <p><strong>תאריך סיום:</strong> {{ \Carbon\Carbon::parse($event->finished_at)->format('dd/mm/YYYY H:i') }}</p>
+        <p><strong>תאריך:</strong> {{ \Carbon\Carbon::parse($event->starts_at)->timezone('Asia/Jerusalem')->format('d/m/Y') }}</p>
+        <p><strong>שעת התחלה:</strong> {{ \Carbon\Carbon::parse($event->starts_at)->timezone('Asia/Jerusalem')->format('H:i') }}</p>
+        <p><strong>תאריך סיום:</strong> {{ \Carbon\Carbon::parse($event->finished_at)->timezone('Asia/Jerusalem')->format('d/m/Y H:i') }}</p>
     </div>
 
     <p>האורחים יכולים כעת להתחיל להעלות תמונות וסרטונים לאירוע.</p>
