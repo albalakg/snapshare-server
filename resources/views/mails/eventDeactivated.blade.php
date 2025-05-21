1de400edddb8623d978c87a9de70aea33f1d0d95
Blacky <gal.blacky@gmail.com>
2025-05-21 22:48:42 +0300

    fix(warning email)

--- resources/views/mails/eventDeactivated.blade.php
+++ resources/views/mails/eventDeactivated.blade.php
@@ -7,7 +7,6 @@
     <div style="background-color: #f8f9fa; padding: 15px; border-radius: 5px; margin: 20px 0;">
         <h3>פרטי האירוע:</h3>
         <p><strong>שם האירוע:</strong> {{ $event->name }}</p>
-        <p><strong>תאריך האירוע:</strong> {{ $event->date->format('d/m/Y') }}</p>
         <p><strong>תאריך השבתה:</strong> {{ now()->format('d/m/Y') }}</p>
     </div>
 
@@ -19,5 +18,6 @@
 
     <div class="success">
         <p>אנו מודים לכם על שבחרתם בנו לאירוע שלכם, מקווים שנהנתם.</p>
+        <p>ומחכים לכם באירוע הבא</p>
     </div>
 @endsection
