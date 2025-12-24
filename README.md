2dcbff5409607acc225b921e45e089b43b9ffe95
Blacky <gal.blacky@gmail.com>
2025-12-25 00:03:00 +0200

    test ini

--- README.md
+++ README.md
@@ -8,9 +8,9 @@
 * Add notification if the event is about to start in 5 days & 1 day and the status is not "ready" - V
 * Add note for deployment link - V
 * Change that the event will be available 8 hours before the start time - V
-* מציג האירוע אינו פעיל - V?
-* לינק לשיתוף אלבום שתשלח למפיקה - V?
-* ⁠לסדר את התמונות של האורחים או התמונות האישיות שישמר - V?
+* מציג האירוע אינו פעיל - V
+* לינק לשיתוף אלבום שתשלח למפיקה - V
+* ⁠לסדר את התמונות של האורחים או התמונות האישיות שישמר - V
 * Show QR in Album - ?
 * Add multiple galleries - ?
 
--- routes/groups/event.php
+++ routes/groups/event.php
@@ -3,6 +3,33 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 
+Route::get('/debug/php-upload-limits', function () {
+    $keys = [
+        'upload_max_filesize',
+        'post_max_size',
+        'memory_limit',
+        'max_execution_time',
+        'max_input_time',
+        'file_uploads',
+        'max_file_uploads',
+        'default_socket_timeout',
+    ];
+
+    $data = [];
+    foreach ($keys as $k) {
+        $data[$k] = ini_get($k);
+    }
+
+    return response()->json([
+        'php_sapi' => php_sapi_name(),
+        'php_version' => PHP_VERSION,
+        'loaded_ini' => php_ini_loaded_file(),        // which php.ini is used
+        'scanned_ini' => php_ini_scanned_files(),     // extra .ini files loaded
+        'ini' => $data,
+    ]);
+});
+
+
 // TODO: for some reason it works only when added in the api.php file as well, need to to check it
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get('{event_id}/assets', [EventController::class, 'assets']);
