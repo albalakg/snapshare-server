6fdf09a9e65a6a299a07aba0dbde392bf497a6fa
Blacky <gal.blacky@gmail.com>
2025-12-25 00:20:34 +0200

    fix

--- app/Http/Requests/UploadFileRequest.php
+++ app/Http/Requests/UploadFileRequest.php
@@ -16,6 +16,9 @@ class UploadFileRequest extends FormRequest
         return [
             'file' => [
                 'required',
+                'file',
+                'max:20480', // 20MB
+                'mimetypes:image/*,video/*',
             ],
         ];
     }
