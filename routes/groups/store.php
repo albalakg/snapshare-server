ba270fac72f9980fda0c443d333537bde7646087
Blacky <gal.blacky@gmail.com>
2026-04-15 23:13:28 +0300

    fix

--- routes/groups/store.php
+++ routes/groups/store.php
@@ -3,8 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StoreController;
 
+Route::post('order-confirmation', [StoreController::class, 'orderCallback']);
+
 Route::group(['middleware' => 'auth:api'], function () {
-    Route::post('order-confirmation', [StoreController::class, 'orderCallback']);
     Route::post('order', [StoreController::class, 'createOrder']);
     Route::post('demo', [StoreController::class, 'createDemo']);
 });
