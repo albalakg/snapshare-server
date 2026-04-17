c712aa2fd6beec69117dfcb0072923b11759b6e7
Blacky <gal.blacky@gmail.com>
2026-04-17 17:23:37 +0300

    fix

--- database/migrations/2026_04_17_140321_add_created_by_guest_to_event_assets.php
+++ database/migrations/2026_04_17_140321_add_created_by_guest_to_event_assets.php
@@ -11,9 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('event_assets', function (Blueprint $table) {
-            $table->boolean('created_by_guest')->default(true)->nullable();
-        });
+        if (!Schema::hasColumn('event_assets', 'created_by_guest')) {
+            Schema::table('event_assets', function (Blueprint $table) {
+                $table->boolean('created_by_guest')->default(true)->nullable();
+            });
+        }
     }
 
     /**
@@ -21,8 +23,10 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('event_assets', function (Blueprint $table) {
-            $table->dropColumn('created_by_guest');
-        });
+        if (Schema::hasColumn('event_assets', 'created_by_guest')) {
+            Schema::table('event_assets', function (Blueprint $table) {
+                $table->dropColumn('created_by_guest');
+            });
+        }
     }
 };
