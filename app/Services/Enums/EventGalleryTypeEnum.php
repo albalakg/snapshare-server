5399dabf7a5b8a0727e504e4e28c4ab62e57376a
Blacky <gal.blacky@gmail.com>
2026-04-17 17:21:36 +0300

    fix

--- app/Services/Enums/EventGalleryTypeEnum.php
+++ app/Services/Enums/EventGalleryTypeEnum.php
@@ -6,7 +6,7 @@ use App\Services\Enums\BaseEnum;
 
 class EventGalleryTypeEnum extends BaseEnum
 {
-  const SINGLE_GALLERY = 'EventSingleGallery',
+  const SINGLE_GALLERY = 'EventGallerySingle',
         RANDOM_GALLERY = 'EventGalleryRandom',        
         SPLIT_GALLERY = 'EventGallerySplitScreen';
 }
