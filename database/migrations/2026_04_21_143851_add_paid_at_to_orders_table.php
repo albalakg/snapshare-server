8c1c00e81a63a3bb29b125f46d4393800f9095a9
Blacky <gal.blacky@gmail.com>
2026-04-21 17:41:30 +0300

    Fix

---
+++ database/migrations/2026_04_21_143851_add_paid_at_to_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (!Schema::hasColumn('orders', 'paid_at')) {
+            Schema::table('orders', function (Blueprint $table) {
+                $table->timestamp('paid_at')->nullable()->after('updated_at');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        if (Schema::hasColumn('orders', 'paid_at')) {
+            Schema::table('orders', function (Blueprint $table) {
+                $table->dropColumn('paid_at');
+            });
+        }
+    }
+};
