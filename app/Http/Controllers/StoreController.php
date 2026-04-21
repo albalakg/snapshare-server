0d0e550e30ec3bb5d1976b049d226eafecad0fc8
Blacky <gal.blacky@gmail.com>
2026-04-21 15:01:47 +0300

    Fix

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -63,14 +63,6 @@ class StoreController extends Controller
 
     public function orderCallback(Request $request)
     {
-        $mail_service = new MailService();
-        $mail_service->send('gal.blacky@gmail.com', \App\Services\Enums\MailEnum::ORDER_CONFIRMED, [
-            'order' => \App\Models\Order::find(42),
-            'first_name' => 'גל בדיקה',
-            'event_url' => config('app.client_url') . '/event',
-        ]);
-        return 1;
-        
         $log = new LogService('payment');
         $log->info('Order callback received', ['data' => $request->all()]);
         
